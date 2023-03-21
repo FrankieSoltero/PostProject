3fe67385b2d945b1b6158fec3f15d8a1cdb84ec9
FrankieSoltero <126137960+FrankieSoltero@users.noreply.github.com>
2023-03-21 12:05:13 -0500

    Add files via upload

---
+++ NPC.java
@@ -0,0 +1,44 @@
+package adventure_game;
+public class NPC extends Character{
+    /**
+     * This is the Constructor for NPC and requires parameters name,
+     * health, mana, baseDamage.
+     * @param name type String
+     * @param health type Int
+     * @param mana type Int
+     * @param baseDamage type Int
+     */
+    public NPC(String name, int health, int level, int baseDamage){
+        super(name, health, level, baseDamage);
+    }
+    /**
+     * This allows for the character to take a turn against the NPC
+     * @param other of type Character
+     */
+    @Override
+    public void takeTurn(Character other){
+        if(this.isStunned()){
+            this.decreaseTurnsStunned();
+            System.out.printf("%S is unable to take any actions this turn!", this.getName());
+            return;
+        }
+        this.attack(other);
+       
+    }
+    @Override
+    /**
+     * This method allows for the player to level up appropriately.
+     * Whenever levelingUpXp is called it levels the player after 3 
+     * kills. Modfies damage and health based on what level it is.
+     */
+    public void levelingUp(){
+        int i;
+        for (i = 0; i < this.level; ++i){
+            int damageModif = (int) (this.baseDamage * 0.1);
+            int healthModif = (int) (this.getHealth() * 0.1);
+            this.maxHealth += healthModif;
+            this.modifyDamage(damageModif);
+            this.modifyHealth(this.maxHealth);
+        }
+    }
+}
